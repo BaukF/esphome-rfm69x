adb80621878f4621406b517b6cd5224dd0be44c3
Bauke <bfahner@gmail.com>
2025-09-11 15:47:33 +0200

    Reverting to a working instance

--- components/duco_rfm69/__init__.py
+++ components/duco_rfm69/__init__.py
@@ -1,19 +1,27 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome import automation, pins
 from esphome.const import CONF_ID
 
-duco_rfm69_ns = cg.esphome_ns.namespace('duco_rfm69')
-DucoRFM69 = duco_rfm69_ns.class_('DucoRFM69', cg.Component, spi.SPIDevice)
+DEPENDENCIES = ["spi"]
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(DucoRFM69),
-    cv.Required("cs_pin"): pins.gpio_output_pin_schema,
-}).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema())
+empty_spi_component_ns = cg.esphome_ns.namespace("duco_rfm69")
+DucoRFM69Compnent = empty_spi_component_ns.class_(
+    "DucoRFM69", cg.Component, spi.SPIDevice
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(DucoRFM69Compnent),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(spi.spi_device_schema(cs_pin_required=True))
+)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
-    cg.add(var.set_cs_pin(await cg.gpio_pin_expression(config["cs_pin"])))
