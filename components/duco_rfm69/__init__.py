fb7dd2c3199ecaff6d95f80deb8b684ac1525605
Bauke <bfahner@gmail.com>
2025-09-11 15:34:20 +0200

    Update __init__.py

--- components/duco_rfm69/__init__.py
+++ components/duco_rfm69/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
 from esphome import automation, pins
+from esphome.const import CONF_ID
 
 duco_rfm69_ns = cg.esphome_ns.namespace('duco_rfm69')
 DucoRFM69 = duco_rfm69_ns.class_('DucoRFM69', cg.Component, spi.SPIDevice)
