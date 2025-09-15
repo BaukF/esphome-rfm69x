8d8ea89c70a71e20db2bde904cda177cb1ecb410
BaukF <bfahner@gmail.com>
2025-09-15 22:34:27 +0200

    Linking issues

--- components/duco_rfm69/duco_rfm69.h
+++ components/duco_rfm69/duco_rfm69.h
@@ -6,23 +6,20 @@
 namespace esphome {
 namespace duco_rfm69 {
 
-// Add a member variable to store the version
+class DucoRFM69 : public Component,
+                  public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST,
+                                        spi::CLOCK_POLARITY_LOW,
+                                        spi::CLOCK_PHASE_LEADING,
+                                        spi::DATA_RATE_1KHZ> {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
 
-
-class DucoRFM69 : public Component, 
-                    public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST,spi::CLOCK_POLARITY_LOW, 
-                            spi::CLOCK_PHASE_LEADING,spi::DATA_RATE_1KHZ> {
-  public:
-    DucoRFM69();
-    void setup() override;
-    void loop() override;
-    void dump_config() override;
-
-  protected:
-      uint8_t version_{0};   // Store the version register value
-      bool detected_{false};
+ protected:
+  uint8_t version_{0};   // Store the version register value
+  bool detected_{false};
 };
-
 
 }  // namespace duco_rfm69
 }  // namespace esphome
