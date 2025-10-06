43432eaa7e7c240c434800672900a578142a4481
BaukF <bfahner@gmail.com>
2025-10-06 21:30:36 +0000

    debug enoug

--- components/rf_sniffer/rf_sniffer.cpp
+++ components/rf_sniffer/rf_sniffer.cpp
@@ -38,7 +38,6 @@ namespace esphome
         if (millis() - last_check > 5000)
         {
           ESP_LOGD("RfSniffer", "Loop running, checking for packets...");
-          this->radio_->dump_config();
           last_check = millis();
         }
 
